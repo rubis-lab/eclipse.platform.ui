960779a9d66f3148d692c29e3206a09e731d6a24
Hendrik Still <hendrik.still@gammas.de>
2013-08-21 15:05:47 +0200

    Bug 415573 - [Viewers] Add generics to the ILazyContentProvider
    Change-Id: Id2812020314fa7cb301d7a1a9b78443f7e3aa575
    Signed-off-by: Hendrik Still <hendrik.still@gammas.de>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ILazyContentProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ILazyContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Hendrik Still <hendrik.still@gammas.de> - bug 415573
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
@@ -14,29 +15,30 @@ package org.eclipse.jface.viewers;
  * The ILazyContentProvider is the content provider
  * for table viewers created using the SWT.VIRTUAL flag that
  * only wish to return their contents as they are queried.
- * 
+ *
  * <strong>NOTE:</strong> As the ILazyContentProvider does
  * not have API for determining the total item count any
  * changes to the number of items for this object while
- * require a call to <code>#setItemCount</code> on the 
+ * require a call to <code>#setItemCount</code> on the
  * viewer that uses it.
+ * @param <I> Type of the input for the view
  */
-public interface ILazyContentProvider extends IContentProvider {
+public interface ILazyContentProvider<I> extends IContentProvider<I> {
 	/**
 	 * Called when a previously-blank item becomes visible in the
 	 * TableViewer. If the content provider knows the element
-	 * at this row, it should respond by calling 
+	 * at this row, it should respond by calling
 	 * TableViewer#replace(Object, int).
-	 * 
+	 *
 	 * <strong>NOTE</strong> #updateElement(int index) can be used to determine selection
-	 * values. TableViewer#replace(Object, int) is not called before 
+	 * values. TableViewer#replace(Object, int) is not called before
 	 * returning from this method selections may have missing or stale elements.
 	 * In this situation it is suggested that the selection is asked
 	 * for again after he update.
-	 * 
+	 *
 	 * @param index The index that is being updated in the
 	 * table.
 	 */
 	public void updateElement(int index);
-	
+
 }
